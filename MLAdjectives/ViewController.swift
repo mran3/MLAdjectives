1a479521bd1a6804bb10abaff4427afe35500275
Andres Acevedo - mran3 <x88andres@hotmail.com>
2020-10-26 18:36:01 -0500

    Initial Commit

---
+++ MLAdjectives/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MLAdjectives
+//
+//  Created by troquer on 10/26/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
